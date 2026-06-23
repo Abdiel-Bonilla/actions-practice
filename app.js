6703469bb420c4f31c6dab39bee3ceb44b5b47a0
Abdiel-Bonilla <abboro123@gmail.com>
2026-06-23 13:29:03 -0600

    add job in workflow 2.0

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 
 console.log("Hello, World!");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(" °=><=° ".repeat(i));
+}
+
  //Obtener variable de entorno
 console.log(process.env.USERNAME);
+
 
 
